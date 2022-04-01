d9f871f760fae765bef966cc4ab05f8413db1b5a
sychen52 <sychen52@gmail.com>
2022-04-01 09:05:39 -0700

    magenta

--- src/DemoPackageCSY.jl
+++ src/DemoPackageCSY.jl
@@ -1,6 +1,6 @@
 module DemoPackageCSY
 
 # Write your package code here.
-print_greeting() = printstyled("Hello, world!"; color = :cyan)
+print_greeting() = printstyled("Hello, world!"; color = :magenta)
 
 end
