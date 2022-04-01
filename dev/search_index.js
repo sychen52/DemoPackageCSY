b43ed48a30cfba64dd92674696c6919d2b3dbb56
Documenter.jl <documenter@juliadocs.github.io>
2022-04-01 05:13:59 +0000

    build based on b629200

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DemoPackageCSY","category":"page"},{"location":"#DemoPackageCSY","page":"Home","title":"DemoPackageCSY","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DemoPackageCSY.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DemoPackageCSY]","category":"page"}]
+}
